dcdb5e4eceb522442b92c43ef9e4a8c0fecaae62
ernesto4664 <eviloria@metasoft-soluciones.cl>
2025-03-11 15:39:18 -0300

    modificaciones

--- src/app/admin/gestion-plandeintervencion/editar-plandeintervencion/editar-plandeintervencion.component.ts
+++ src/app/admin/gestion-plandeintervencion/editar-plandeintervencion/editar-plandeintervencion.component.ts
@@ -7,11 +7,13 @@ import { TerritoriosService } from '../../../services/territorios.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { BehaviorSubject } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-editar-plandeintervencion',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule,MatSelectModule, MatFormFieldModule],
+  imports: [CommonModule, ReactiveFormsModule,MatSelectModule, MatFormFieldModule,MatButtonModule,MatIconModule],
   templateUrl: './editar-plandeintervencion.component.html',
   styleUrl: './editar-plandeintervencion.component.scss'
 })
