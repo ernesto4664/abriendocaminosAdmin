048fe3b6bdc7b5498167cdaa8820e3150124f312
Ubuntu <ubuntu@ip-172-31-14-173.us-east-2.compute.internal>
2025-05-02 14:25:14 +0000

    xIgnorar archivos de entorno locales

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiBaseUrl: 'http://127.0.0.1:8000/api/v1',
-    apiLineaUrl: 'http://127.0.0.1:8000/api/v1'
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiBaseUrl: 'http://127.0.0.1:8000/api/v1',
-    apiLineaUrl: 'http://127.0.0.1:8000/api/v1'
-  };
-  
